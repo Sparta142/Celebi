ccb8514f85b3ae12e88131a3e2b79b8d4a8b9fdd
Sparta142 <Sparta142@users.noreply.github.com>
2023-11-10 02:22:41 -0500

    Add more fields to models.Member

--- celebi/astonish/models.py
+++ celebi/astonish/models.py
@@ -1,7 +1,7 @@
 from typing import Self
 
 from bs4 import BeautifulSoup, Tag
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from yarl import URL
 
 
@@ -49,4 +49,41 @@ class Pokemon(BaseModel):
 
 
 class Member(BaseModel):
-    personal_computer: list[Pokemon]
+    title: str
+    website: str
+    location: str
+    interests: str
+    signature: str
+    full_name: str = Field(alias='field_1')
+    nicknames: str = Field(alias='field_2')
+    age: str = Field(alias='field_3')
+    date_of_birth: str = Field(alias='field_4')
+    gender_and_pronouns: str = Field(alias='field_5')
+
+    # TODO: Missing fields 6-7
+
+    orientation: str = Field(alias='field_8')
+    marital_status: str = Field(alias='field_9')
+    height: str = Field(alias='field_10')
+    occupation: str = Field(alias='field_11')
+    home_region: str = Field(alias='field_12')
+    face_claim: str = Field(alias='field_13')
+    art_credits: str = Field(alias='field_14')
+    flavour_text: str = Field(alias='field_15')
+    biography: str = Field(alias='field_16')
+    plot_page: str = Field(alias='field_17')
+    player_name: str = Field(alias='field_18')
+    player_pronouns: str = Field(alias='field_19')
+
+    # TODO: Missing fields 20-24
+
+    personal_computer: list[Pokemon] = Field(alias='field_25')
+    inamorata_ability: str = Field(alias='field_26')
+
+    # TODO: Missing fields 27-30
+
+    development_forum: str = Field(alias='field_31')
+
+    @field_validator('personal_computer', mode='after')
+    def _validate_personal_computer(cls, v: str) -> list[Pokemon]:
+        return Pokemon.parse_html(v)
